ab3001a81043eefdd60280915c338631dee940a3
manminder singh <bewithme2407@gmail.com>
2024-07-04 12:52:16 +0530

    add

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,6 +5,7 @@ import grD from '../assets/bg-main-desktop.png'
 import grM from '../assets/bg-main-mobile.png'
 import cdF from '../assets/bg-card-front.png'
 import cdB from '../assets/bg-card-back.png'
+import api from '../axios/axios_config';
 
 function Signup() {
   const navigate = useNavigate();
